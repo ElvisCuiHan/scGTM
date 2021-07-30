853344939ae025841797c8f660e4d31812a85f52
Elvis Cui <elviscuihan@g.ucla.edu>
2021-07-30 18:01:01 +0800

    version 0.2.0

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scBCM",
+    version="0.2",
+    author="Elvis Cui",
+    author_email="elviscuihan@g.ucla.edu",
+    description="Bell-shape Curve Model.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ElvisCuiHan/scBCM",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
