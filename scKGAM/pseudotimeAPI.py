3a2dbf70d20f602c19826a754de284315c074fee
Elvis Cui <44211579+ElvisCuiHan@users.noreply.github.com>
2021-08-03 11:25:20 +0800

    switch scBCM to scKGAM

---
+++ scKGAM/pseudotimeAPI.py
@@ -0,0 +1,192 @@
+import numpy as np
+from scipy.stats import nbinom, poisson
+import matplotlib.pyplot as plt
+
+## Shared parameters log-bell
+def link(t, mu, k1, k2, t0):
+    """
+
+    :param t:
+    :param mu:
+    :param k1:
+    :param k2:
+    :param t0:
+    :return:
+    """
+    part1 = 2 * mu * np.exp(- np.abs(k1) * (t - t0) ** 2) * np.sign(k1)
+    part2 = 2 * mu * np.exp(- np.abs(k2) * k2 * (t - t0) ** 2) * np.sign(k2)
+
+    return part1 * (t <= t0) + part2 * (t > t0)
+
+## Poisson
+def single_gene_log_likelihood_Poisson(y, t, mu, k1, k2, t0):
+    """
+
+    :param y:
+    :param t:
+    :param mu:
+    :param k1:
+    :param k2:
+    :param t0:
+    :return:
+    """
+    bell = link(t, mu, k1, k2, t0)
+    mut = np.maximum(np.exp(bell) , 0.1)
+    return -np.log(poisson.pmf(y, mut) + 1e-300).sum()
+
+## Zero-inflated Poisson
+def single_gene_log_likelihood_ZIP(y, t, mu, k1, k2, t0, alpha, beta):
+    """
+
+    :param y:
+    :param t:
+    :param mu:
+    :param k1:
+    :param k2:
+    :param t0:
+    :param p:
+    :return:
+    """
+    bell = link(t, mu, k1, k2, t0)
+    mut = np.maximum(np.exp(bell), 0.1)
+    cache = poisson.pmf(y, mut) + 1e-300
+
+    ## Zero-inflation
+    p = 1 / (1 + np.exp(alpha * np.log(mut) + beta))
+
+    return - np.log(cache * (1 - p) + p * (y == 0)).sum()
+
+## Negative Binomial
+def single_gene_log_likelihood_NB(y, t, mu, k1, k2, t0, phi):
+    """
+
+    :param y:
+    :param t:
+    :param mu:
+    :param k1:
+    :param k2:
+    :param t0:
+    :param phi:
+    :return:
+    """
+    bell = link(t, mu, k1, k2, t0)
+    mut = np.maximum(np.exp(bell) , 0.1)
+    phi = np.maximum(np.floor(phi), 1)
+    p0 = mut / (mut + phi)
+    cache = nbinom.pmf(y, phi, 1 - p0) + 1e-300
+    return -np.log(cache).sum()
+
+## Zero-inflated Negative Binomial
+def single_gene_log_likelihood_ZINB(y, t, mu, k1, k2, t0, phi, alpha, beta):
+    """
+
+    :param y:
+    :param t:
+    :param mu:
+    :param k1:
+    :param k2:
+    :param t0:
+    :param phi:
+    :param p:
+    :return:
+    """
+    bell = link(t, mu, k1, k2, t0)
+    mut = np.maximum(np.exp(bell) , 0.01)
+    phi = np.maximum(np.floor(phi), 1)
+    p0 = phi / (mut + phi)
+    cache = nbinom.pmf(y, phi, p0) + 1e-300
+
+    ## Zero-inflation
+    p = 1 / (1 + np.exp(alpha * np.log(mut) + beta))
+
+    return - np.log(cache * (1 - p) + p * (y == 0)).sum()
+
+def pso_obj_fct(b, **kwargs):
+    """
+
+    :param b: an Nxd particle matrix where N is the number of particles,
+              and d is the dimension of each particle.
+    :param kwargs:
+    :return:
+    """
+    N, d = b.shape
+    y, t, marginal = kwargs.values()
+
+    cost = np.zeros(N)
+    for i in range(N):
+        if marginal == "ZINB":
+            mu, k1, k2, t0, phi, alpha, beta = b[i, :]
+            cost[i] = single_gene_log_likelihood_ZINB(y, t, mu, k1, k2, t0, phi, alpha, beta)
+        elif marginal == "NB":
+            mu, k1, k2, t0, phi, alpha, beta = b[i, :]
+            cost[i] = single_gene_log_likelihood_NB(y, t, mu, k1, k2, t0, phi)
+        elif marginal == "ZIP":
+            mu, k1, k2, t0, alpha, beta = b[i, :]
+            cost[i] = single_gene_log_likelihood_ZIP(y, t, mu, k1, k2, t0, alpha, beta)
+        else:
+            mu, k1, k2, t0, alpha, beta = b[i, :]
+            cost[i] = single_gene_log_likelihood_Poisson(y, t, mu, k1, k2, t0)
+    return cost
+
+## Plot the results
+def plot_result(para, t, color, marginal):
+    mu_fit, k1_fit, k2_fit, t0_fit = para[:4]
+    log_mut_fit = link(np.sort(t), mu_fit, k1_fit, k2_fit, t0_fit)
+
+    p_fit = 1 / (1 + np.exp(para[-1] + para[-2] * np.exp(log_mut_fit)))
+
+    ZIlog_mut_fit = np.maximum(log_mut_fit + np.log(1 - p_fit), 0)
+
+    plt.plot(np.sort(t), log_mut_fit, linewidth=3, c=color[0], label="Fitted curve (mean function)")
+    if marginal in ["ZIP", "ZINB"]:
+        plt.plot(np.sort(t), ZIlog_mut_fit - 0.1, linewidth=1.2, c=color[1], label="With dropout (minus -log(1-p))")
+    plt.xlabel("Pseudotime")
+    plt.ylabel("Expression log(FPKM+1)")
+    if t0_fit <= 1 and t0_fit >= 0:
+        plt.axvline(t0_fit, linewidth=4, color=color[2], linestyle="--",
+                    ymin=0.02, ymax=0.96, label="Estimated t0 (turning point)")
+    plt.legend()
+
+    if marginal in ["ZIP", "ZINB"]:
+        plt.twinx()
+        plt.ylabel("Dropout Rate", c=color[3], fontsize=14)
+        plt.plot(np.sort(t), p_fit, linewidth=1.5, c=color[3], label="Dropout rate")
+
+def Fisher_info(t, para, marginal):
+    """
+    This function calculates the Fisher information matrix of parameters of interest.
+    Specifically, mu, k1, k2 and t0. Zero-inflation parameter p will also be included
+    if the marginal is either ZIP or ZINB.
+    :param t: Pseudotime, should be a vector.
+    :param para: Estimated parameters, should be a vector.
+    :param marginal: Marginal distribution, should be one of ["Poisson", "NB", "ZIP", "ZINB"].
+    :return: a 4x4 Fisher information matrix.
+    """
+    if marginal == "ZIP" or marginal == "Poisson":
+        mu_fit, k1_fit, k2_fit, t0_fit, alpha_fit, beta_fit = para
+    else:
+        mu_fit, k1_fit, k2_fit, t0_fit, phi_fit, alpha_fit, beta_fit = para
+    log_mut_fit = link(t, mu_fit, k1_fit, k2_fit, t0_fit)
+
+    mut = np.maximum(np.exp(log_mut_fit), 0.1)
+
+    t0_deri = 2 * (k1_fit * (t <= t0_fit) + k2_fit * (t > t0_fit)) * (t - t0_fit) * log_mut_fit  # * (1 + 1/mut)
+    k1_deri = (t - t0_fit) ** 2 * log_mut_fit * (t <= t0_fit)  # * (1 + 1/mut)
+    k2_deri = (t - t0_fit) ** 2 * log_mut_fit * (t > t0_fit)  # * (1 + 1/mut)
+    mu_deri = log_mut_fit / mu_fit  # * (1 + 1/mut)
+
+    if marginal == "Poisson":
+        t0_deri *= (1 + 1 / mut); k1_deri *= (1 + 1 / mut)
+        k2_deri *= (1 + 1 / mut); mu_deri *= (1 + 1 / mut)
+    elif marginal == "ZIP":
+        t0_deri *= (1 + 1 / mut); k1_deri *= (1 + 1 / mut)
+        k2_deri *= (1 + 1 / mut); mu_deri *= (1 + 1 / mut)
+    else:
+        t0_deri *= phi_fit * (mut + 1) / (mut + phi_fit)
+        k1_deri *= phi_fit * (mut + 1) / (mut + phi_fit)
+        k2_deri *= phi_fit * (mut + 1) / (mut + phi_fit)
+        mu_deri *= phi_fit * (mut + 1) / (mut + phi_fit)
+
+    cache = np.vstack([t0_deri, k1_deri, k2_deri, mu_deri])
+
+    return (cache.dot(cache.T))
---
+++ scKGAM/scKGAM.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import json
+import numpy as np
+import pyswarms as ps
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+from scipy.stats import nbinom, poisson
+from pseudotimeAPI import *
+from pseudotimeEstInfer import *
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def main(gene_index = 100, marginal="ZIP", iter=50, data_dir=None, save_dir=None, plot_args=None):
+
+    print("Loading data......")
+
+    ## LOAD DATA
+    data = pd.read_csv(data_dir)
+    print("Loading finished!")
+
+    ## TAKE NEEDED DATA
+    t = data.iloc[:, 1]
+    y1 = np.floor(data.iloc[:, gene_index])
+    gene_name = data.columns[gene_index]
+
+    ## ESTIMATION
+    print("\nWe are estimating gene %d with marginal %s." % (gene_index, marginal))
+
+    result = {}
+    gcost, gbest = estimation(y1, t, marginal, iter)
+    result['negative_log_likelihood'] = gcost
+
+    if gcost > 1e2:
+        print("\nBest negative log-likelihood: ", np.round(gcost, 2), "\n")
+    else:
+        print("\nAlgorithm fails to find reasonable estimation.\n")
+
+    if marginal == "ZIP":
+        result['mu'] = gbest[0]; result['k1'] = gbest[1]
+        result['k2'] = gbest[2]; result['t0'] = gbest[3]; result['phi'] = "Nah"
+        result['alpha'] = gbest[4]; result['beta'] = gbest[5]
+
+
+        print("Best parameter estimation:\n",
+              "mu , k1 , k2 , t0 , p:\n",
+              np.round(gbest, 2), "\n")
+    elif marginal == "ZINB":
+        gbest[4] = np.maximum(np.floor(gbest[-2]), 1)
+        result['mu'] = gbest[0]; result['k1'] = gbest[1]; result['k2'] = gbest[2]
+        result['t0'] = gbest[3]; result['phi'] = gbest[4]; result['alpha'] = gbest[5]; result['beta'] = gbest[6]
+
+        print("Best parameter estimation:\n",
+              "mu , k1 , k2 , t0 , phi , p:\n",
+              np.round(gbest, 2), "\n")
+    elif marginal == "Poisson":
+        result['mu'] = gbest[0]
+        result['k1'] = gbest[1]
+        result['k2'] = gbest[2]
+        result['t0'] = gbest[3]
+        result['phi'] = "Nah"
+        result['p'] = "Nah"
+
+        print("Best parameter estimation:\n",
+              "mu , k1 , k2 , t0:\n",
+              np.round(gbest[:-1], 2), "\n")
+    else:
+        gbest[-2] = np.maximum(np.floor(gbest[-2]), 1)
+        result['mu'] = gbest[0]
+        result['k1'] = gbest[1]
+        result['k2'] = gbest[2]
+        result['t0'] = gbest[3]
+        result['phi'] = gbest[4]
+        result['p'] = "Nah"
+
+        print("Best parameter estimation:\n",
+              "mu , k1 , k2 , t0 , phi:\n",
+              np.round(gbest[:-1], 2), "\n")
+
+    ## PLOTTING
+    if plot_args is not None:
+        color = plot_args['color']
+        cmap = plot_args['cmap']
+    else:
+        color = ['red', 'blue', 'orange', 'darkgreen']
+        cmap = 'PRGn'
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    log_data = np.log(y1 + 1)
+    plt.scatter(t, log_data, s=10, c=log_data, cmap=plt.get_cmap(cmap))
+    plt.ylim(np.min(log_data) - 1, np.max(log_data) + 1)
+
+    plot_result(gbest, t, color, marginal=marginal)
+
+    plt.title("Marginal: " + marginal + ". Gene: " + str(gene_name) + '.', fontsize=15)
+    #plt.show()
+
+    plt.savefig(save_dir + str(gene_index) + marginal + ".png")
+
+    ## FISHER INFORMATION
+    fisher, var, t0_lower, t0_upper = inference(t, gbest, marginal)
+    result['t0_lower'] = t0_lower
+    result['t0_upper'] = t0_upper
+
+    #print("Inverse Fisher information matrix of first 4 parameters or t0 alone:\n",
+    #      var , "\n")
+
+    ## CONFIDENCE INTERVAL
+    print("The 95% confidence interval of the activation time t0:\n" +
+          "t0 : (" + str(t0_lower) + ", " + str(t0_upper) + ")")
+
+    result['k1_lower'] = result['k1_upper'] = result['k2_lower'] = \
+        result['k2_upper'] = result['mu_lower'] = result['mu_upper'] =  "Nah"
+
+    if np.ndim(var) > 1:
+        k1_lower = np.round(gbest[1] - 1.96 * np.sqrt(var[1, 1]), 3)
+        k1_upper = np.round(gbest[1] + 1.96 * np.sqrt(var[1, 1]), 3)
+        k2_lower = np.round(gbest[2] - 1.96 * np.sqrt(var[2, 2]), 3)
+        k2_upper = np.round(gbest[2] + 1.96 * np.sqrt(var[2, 2]), 3)
+        mu_lower = np.round(gbest[0] - 1.96 * np.sqrt(var[3, 3]), 3)
+        mu_upper = np.round(gbest[0] + 1.96 * np.sqrt(var[3, 3]), 3)
+        print("\nThe 95% CIs for activation strength k1 and k2:\n" +
+            " k1 : (" + str(k1_lower) + ", " + str(k1_upper) + ")\n",
+            "k2 : (" + str(k2_lower) + ", " + str(k2_upper) + ")\n"
+             )
+        result['k1_lower'] = k1_lower; result['k1_upper'] = k1_upper
+        result['k2_lower'] = k2_lower; result['k2_upper'] = k2_upper
+        result['mu_lower'] = mu_lower; result['mu_upper'] = mu_upper
+        result['Fisher'] = 'Non-singular'
+    else:
+        var = fisher
+        var[0, 0] = 1 / (var[0, 0] + 1e-100)
+        var[1, 1] = 1 / (var[1, 1] + 1e-100)
+        var[2, 2] = 1 / (var[2, 2] + 1e-100)
+        var[3, 3] = 1 / (var[3, 3] + 1e-100)
+        k1_lower = np.round(gbest[1] - 1.96 * np.sqrt(var[1, 1]), 3)
+        k1_upper = np.round(gbest[1] + 1.96 * np.sqrt(var[1, 1]), 3)
+        k2_lower = np.round(gbest[2] - 1.96 * np.sqrt(var[2, 2]), 3)
+        k2_upper = np.round(gbest[2] + 1.96 * np.sqrt(var[2, 2]), 3)
+        mu_lower = np.round(gbest[0] - 1.96 * np.sqrt(var[3, 3]), 3)
+        mu_upper = np.round(gbest[0] + 1.96 * np.sqrt(var[3, 3]), 3)
+        print("\nThe 95% CIs for activation strength k1 and k2:\n" +
+            " k1 : (" + str(k1_lower) + ", " + str(k1_upper) + ")\n",
+            "k2 : (" + str(k2_lower) + ", " + str(k2_upper) + ")\n"
+             )
+        result['k1_lower'] = k1_lower; result['k1_upper'] = k1_upper
+        result['k2_lower'] = k2_lower; result['k2_upper'] = k2_upper
+        result['mu_lower'] = mu_lower; result['mu_upper'] = mu_upper
+        result['Fisher'] = 'Singular'
+
+    ## SAVE ESTIMATION RESULTS
+    with open(save_dir + str(gene_index) + marginal + '.json', 'w') as fp:
+        json.dump(result, fp)
+
+    return result
+
